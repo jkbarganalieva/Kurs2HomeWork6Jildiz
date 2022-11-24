756fb6eda1be6ebcb230a7f28cdfefb2a49e223f
jkbarganalieva <barganalieva@mail.ru>
2022-11-24 17:49:40 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/kurs2homework6jildiz/MainActivity.java
@@ -0,0 +1,76 @@
+package com.geektech.kurs2homework6jildiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        EditText gMail = findViewById(R.id.edit_text);
+        TextView log = findViewById(R.id.log);
+        TextView tv = findViewById(R.id.text_view_3);
+        EditText password = findViewById(R.id.edit_text_2);
+        Button button = findViewById(R.id.big_button);
+
+
+        button.setOnClickListener(view -> {
+            if (!gMail.getText().toString().equals("Admin") && !gMail.getText().toString().equals("Admin")) {
+                Toast.makeText(this, "Пароль не верный!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Вы успешно вошли!", Toast.LENGTH_SHORT).show();
+                gMail.setVisibility(View.INVISIBLE);
+                log.setVisibility(View.INVISIBLE);
+                password.setVisibility(View.INVISIBLE);
+                button.setVisibility(View.INVISIBLE);
+                tv.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+       gMail.addTextChangedListener(new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence s,int start, int count, int after) {
+
+           }
+
+
+
+
+           @Override
+           public void onTextChanged(CharSequence s,int start, int count, int after) {
+
+               if (!s.toString().isEmpty() || !password.getText().toString().isEmpty()){
+                   button.setBackgroundColor(Color.parseColor("#4C4A4C"));
+               }else {
+                   button.setBackgroundColor(Color.parseColor("#F65B00"));
+               }
+           }
+
+           @Override
+           public void afterTextChanged(Editable s) {
+
+           }
+       });
+
+
+    }
+}
